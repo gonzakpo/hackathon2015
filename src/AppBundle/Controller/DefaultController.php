f21bcd22a35fff3925cd915a9ba59d4d3b9453de
Alejandro Gomez Santillan <alejandro@42mate.com>
2015-08-29 17:04:09 -0300

    pagina esculturas renderizadas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Hateoas\HateoasBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,64 +11,66 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
 	/**
-     * @Route("/app/example", name="homepage")
-     */
-    public function indexAction()
-    {
-    	$esculturas = $this->traerNodos();
-    	
-    	ladybug_dump_die($esculturas);
-		//var_dump($nds);
-		//die();
-        return $this->render('AppBundle::Puzzle/puzzle.html.twig' array(
-            'esculturas' => $esculturas,
-            );
-    }
+   * @Route("/", name="homepage")
+   */
+  public function indexAction() {
+
+    return $this->render('AppBundle::Puzzle/puzzle.html.twig');
+  }
 
 
+  public function traerNodos(){
+    $url = 'http://www.resistenciarte.org/api/v1/node?pagesize=20';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+      $data = json_decode($response, true);
+      $arrayEsculturas = array();
+      foreach ($data as $key => $nodo) {
+        if ($nodo["type"] == "escultura"){
+          $titulo = "no tiene";
+          $foto = "no tiene";
+          $escultura = $this->traerEscultura($nodo["uri"]);
+          //ladybug_dump_die($escultura["field_fotos"]);
+          if ($escultura["title"]){
+            $titulo = $escultura["title"];
+          }
+          if ($escultura["field_fotos"]["und"][0]["filename"]){
+            $foto = "http://www.resistenciarte.org/sites/resistenciarte.org/files/" . $escultura["field_fotos"]["und"][0]["filename"];
+          }
+          $arrayEsculturas[$key] = array(
+            'titulo' => $titulo,
+            'imgUrl' => $foto,
+          );
+        }
+      }
 
-    public function traerNodos(){
-    	$url = 'http://www.resistenciarte.org/api/v1/node?pagesize=100';
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$response = curl_exec($ch);
-    	$data = json_decode($response, true);
-    	$arrayEsculturas = array();
-    	foreach ($data as $key => $nodo) {
-    		if ($nodo["type"] == "escultura"){
-    			$titulo = "no tiene";
-    			$foto = "no tiene";
-    			$escultura = $this->traerEscultura($nodo["uri"]);
-    			//ladybug_dump_die($escultura["field_fotos"]);
-    			if ($escultura["title"]){
-    				$titulo = $escultura["title"];
-    			}
-    			if ($escultura["field_fotos"]["und"][0]["filename"]){
-    				$foto = "
-http://www.resistenciarte.org/sites/resistenciarte.org/files/" . $escultura["field_fotos"]["und"][0]["filename"];
-    			}
-    			$arrayEsculturas[$key] = array(
-    				'titulo' => $titulo,
-    				'imgUrl' => $foto,
-    			);
-    		}
-    	}
+    return $arrayEsculturas;
+  }
 
-    	return $arrayEsculturas;
-    }
+  public function traerEscultura($url){
 
-    public function traerEscultura($url){
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  $response = curl_exec($ch);
+    $data = json_decode($response, true);
 
-		$response = curl_exec($ch);
-    	$data = json_decode($response, true);
-    	
-    	return $data;
-    }
+    return $data;
+  }
+
+  /**
+   * @Route("/esculturas", name="esculturas")
+   */
+  public function esculturasAction() {
+    $esculturas = $this->traerNodos();
+
+    return $this->render('AppBundle::Esculturas/esculturas.html.twig', array(
+      'esculturas' => $esculturas,
+    ));
+  }
 }
